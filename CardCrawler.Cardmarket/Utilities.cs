32093ea49084148de4fb5e35c02cc77dfaf14459
DavidSDot <struthers.david@gmail.com>
2025-05-10 11:23:17 +0200

    Update Utilities and modify card lists in Deck and exclude
    - Enhanced `Utilities.cs` with new using directives and introduced `CardNameExtractionRegex`.
    - Removed several regex fields while modifying `CleanCardName` and `UrlEncodeCardName` methods.

--- CardCrawler.Cardmarket/Utilities.cs
+++ CardCrawler.Cardmarket/Utilities.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,26 +9,19 @@ namespace CardCrawler.Cardmarket
 {
     public static partial class Utilities
     {
-        private static readonly Regex CleanCardNameRegex = new(@"^\d+\s+([^\(]+)", RegexOptions.Compiled);
-        private static readonly Regex RemovePrefixesRegex = new(@"^\d+\s*[xX]?\s*", RegexOptions.Compiled);
-        private static readonly Regex RemoveParenthesesAndSuffixRegex = new(@"\s*\(.*?\)\s*\d*$", RegexOptions.Compiled);
-        private static readonly Regex RemoveParenthesesRegex = new(@"\s*\(.*?\)", RegexOptions.Compiled);
-        private static readonly Regex RemoveAsteriskSuffixRegex = new(@"\s*\*.*", RegexOptions.Compiled);
-        private static readonly Regex RemoveSlashesRegex = new(@"\s*\/", RegexOptions.Compiled);
-        private static readonly Regex UrlEncodeRegex = new(@"[^a-zA-Z0-9\-]", RegexOptions.Compiled);
 
         [GeneratedRegex(@"^(?:\d*[x|X]*\s+)(?<name>.*?)(?:\s\(.*)?$", RegexOptions.Singleline)]
         private static partial Regex CardNameExtractionRegex();
 
         public static string CleanCardName(string cardName)
         {
-            return CardNameExtractionRegex().Match(cardName).Groups["name"].Value;
+           return CardNameExtractionRegex().Match(cardName).Groups["name"].Value;
         }
 
         public static string UrlEncodeCardName(string cardName)
         {
-            cardName = string.Join(" ", Regex.Matches(cardName, @"\w+").Select(m => m.Value));  // Remove invalid characters
-            cardName = Regex.Replace(cardName, @"\s", "-");   // Remove hyphens
+            cardName = string.Join(" ", Regex.Matches(cardName, @"\w+").Select(m => m.Value));
+            cardName = Regex.Replace(cardName, @"\s", "-");
             return cardName;
         }
 
